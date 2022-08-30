bbd37e7556811ec8aaaecb97f9b1c9a895bd9ffb
arthur <aenartzr@gmail.com>
2022-08-30 15:54:41 -0300

    fix(Codec): codec class had private attributes. Changed to protected as this is a super class.

--- code/codecs/Codec.py
+++ code/codecs/Codec.py
@@ -3,20 +3,20 @@ from abc import ABC, abstractclassmethod, abstractmethod
 class Codec(ABC):
     @abstractmethod
     def __init__(self,bit_stream_path,output_path):
-        self.__bit_stream_path = bit_stream_path
-        self.__output_path = output_path
+        self._bit_stream_path = bit_stream_path
+        self._output_path = output_path
 
     def get_bit_stream_path(self):
-        return self.__bit_stream_path
+        return self._bit_stream_path
 
     def set_bit_stream_path(self, bit_stream_path):
-        self.__bit_stream_path = bit_stream_path
+        self._bit_stream_path = bit_stream_path
 
     def get_output_path(self):
-        return self.__output_path
+        return self._output_path
 
     def set_output_path(self, output_path):
-        self.__output_path = output_path
+        self._output_path = output_path
 
     @abstractmethod
     def encode(self):
