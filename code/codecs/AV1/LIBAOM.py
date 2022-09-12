5cafae36f9fdfa5e66695bb0212a9ecc1ad020b5
EduLodi <f.eduardo.lodi@gmail.com>
2022-09-12 12:21:07 -0300

    Fixed some issues, moved some function to their classes, start of standardization of JSON files

--- code/codecs/AV1/LIBAOM.py
+++ code/codecs/AV1/LIBAOM.py
@@ -14,10 +14,10 @@ class libaom_codec:
         self.name = self.__raw["name"]
 
     def encode(self):
-        aompath = self.data['paths']["libaom_enc"]
-        options_aome = self.data['paths']["libaom_options_enc"]
-        encoded_out = self.data['paths']["libaom_encoded"] +"/encaom_"+self.name
-        outgen = self.data['paths']["libaom_outgen"] +"/aom"+self.name+".log"
+        aompath = self.data['paths']["encoder"]
+        options_aome = self.data['paths']["options_encoder"]
+        encoded_out = self.data['paths']["encoded"] +"/encaom_"+self.name
+        outgen = self.data['paths']["outgen"] +"/aom"+self.name+".log"
         cmdline = aompath + ' --verbose --psnr ' + options_aome 
         cmdline += ' -o ' + encoded_out + ' ' + self.__raw["path"]
         cmdline += " > "+ outgen +" 2>&1"
@@ -25,23 +25,23 @@ class libaom_codec:
         os.system(cmdline)
 
     def decode(self):
-        aompath = self.data['paths']['libaom_dec']
-        options_libaom = self.data['paths']['libaom_options_dec']
-        encoded_out = self.data['paths']['libaom_encoded'] +"/encaom_"+self.name
-        decoded_out = self.data['paths']['libaom_bitstream'] +"/decaom_"+self.name
+        aompath = self.data['paths']['decoder']
+        options_libaom = self.data['paths']['options_decoder']
+        encoded_out = self.data['paths']['encoded'] +"/encaom_"+self.name
+        decoded_out = self.data['paths']['bitstream'] +"/decaom_"+self.name
         cmdline = aompath + ' ' + options_libaom + ' ' + encoded_out + ' -o ' + decoded_out
         print(cmdline)
         os.system(cmdline)
 
     def parse(self):
-        outgen = self.data['paths']['libaom_outgen'] +"/aom"+self.name
+        outgen = self.data['paths']['outgen'] +"/aom"+self.name
         p = Path('~').expanduser()
         outgen = outgen.replace("~",str(p))
         bitrate, psnr, timems = av1_outparse.parse_aom_output(outgen)
         return bitrate,psnr,timems
 
     def parsed2csv(self):
-        outputcsvpapth = self.data['paths']['libaom_outcsv']
+        outputcsvpapth = self.data['paths']['outcsv']
         outputcsv = outputcsvpapth + '/' + self.name + ".csv"
         p = Path('~').expanduser()
         outputcsv = outputcsv.replace("~",str(p))
@@ -49,10 +49,4 @@ class libaom_codec:
         with open(outputcsv, 'w', newline='') as metrics_file:
             metrics_writer = csv.writer(metrics_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             metrics_writer.writerow(['encoder','video','resolution','fps','number of frames','qp','bitrate', 'psnr', 'timems','optional settings'])
-            metrics_writer.writerow(["LIBAOM-AV1",self.name,self.__raw["resolution"],self.__raw["fps"],self.__raw["framesnumber"],'',bitrate,psnr,timems,self.data["paths"]["libaom_options_enc"]])
-
-
-x = libaom_codec('code/codecs/AV1/JSON_files/video.JSON', 'code/codecs/AV1/JSON_files/libaom.JSON')
-x.encode()
-x.decode()
-x.parsed2csv()
+            metrics_writer.writerow(["LIBAOM-AV1",self.name,self.__raw["resolution"],self.__raw["fps"],self.__raw["framesnumber"],'',bitrate,psnr,timems,self.data["paths"]["options_encoder"]])
--- code/codecs/AV1/SVT.py
+++ code/codecs/AV1/SVT.py
@@ -1,6 +1,5 @@
 import os
 import json
-import av1_outparse
 import csv
 from pathlib import Path
 
@@ -15,36 +14,36 @@ class svt_codec:
         
 
     def encode(self):
-        svtpath = self.data["paths"]["svt_enc"]
-        options_svte = self.data["paths"]["svt_options_enc"]
-        encoded_out = self.data["paths"]["svt_encoded"]+"/svtenc_"+self.name
-        outgen = self.data["paths"]["svt_outgen"]+"/"+self.name+".log"
-        outtime = self.data["paths"]["svt_outtime"]+"/"+self.name+".txt"
+        svtpath = self.data["paths"]["encoder"]
+        options_svte = self.data["paths"]["options_encoder"]
+        encoded_out = self.data["paths"]["encoded"]+"/svtenc_"+self.name
+        outgen = self.data["paths"]["outgen"]+"/"+self.name+".log"
+        outtime = self.data["paths"]["outtime"]+"/"+self.name+".txt"
         cmdline = svtpath + ' --enable-stat-report 1 --stat-file ' + outgen  + ' ' + options_svte
         cmdline += ' -i ' + self.__raw["path"] + ' -b ' + encoded_out + ' 2> ' + outtime 
         print(cmdline)
         os.system(cmdline)
 
     def decode(self):
-        svtpath = self.data["paths"]["svt_dec"]
-        options_svtd = self.data["paths"]["svt_options_dec"]
-        encoded_out = self.data["paths"]["svt_encoded"]+"/svtenc_"+self.name
-        decoded_out = self.data["paths"]['svt_bitstream']+"/svtdec_"+self.name
+        svtpath = self.data["paths"]["decoder"]
+        options_svtd = self.data["paths"]["options_decoder"]
+        encoded_out = self.data["paths"]["encoded"]+"/svtenc_"+self.name
+        decoded_out = self.data["paths"]['bitstream']+"/svtdec_"+self.name
         cmdline = (svtpath + ' ' + options_svtd + ' -i ' + encoded_out + ' -o ' + decoded_out)
         print(cmdline)
         os.system(cmdline)
 
     def parse(self):
-        outgen = self.data["paths"]["svt_outgen"]+"/"+self.name+".log"
-        outtime = self.data["paths"]["svt_outtime"]+"/"+self.name+".txt"
+        outgen = self.data["paths"]["outgen"]+"/"+self.name+".log"
+        outtime = self.data["paths"]["outtime"]+"/"+self.name+".txt"
         p = Path('~').expanduser()
         outgen=outgen.replace("~",str(p))
         outtime=outtime.replace("~",str(p))
-        bitrate, psnr, timems = av1_outparse.parse_svt_output(outgen,outtime)
+        bitrate, psnr, timems = self.parse_svt_output(outgen,outtime)
         return bitrate, psnr, timems
 
     def parsed2csv(self):
-        outputcsvpapth = self.data['paths']['svt_outcsv']
+        outputcsvpapth = self.data['paths']['outcsv']
         outputcsv = outputcsvpapth + '/' + self.name + ".csv"
         print(outputcsv)
         bitrate,psnr,timems = self.parse()
@@ -55,8 +54,18 @@ class svt_codec:
             metrics_writer.writerow(['encoder','video','resolution','fps','number of frames','qp','bitrate', 'psnr', 'timems','optional settings'])
             metrics_writer.writerow(["SVT-AV1",self.name,self.__raw["resolution"],self.__raw["fps"],self.__raw["framesnumber"],'',bitrate,psnr,timems,self.data["paths"]["svt_options_enc"]])
 
+    def parse_svt_output(pt1,pt2):
 
-x = svt_codec('code/codecs/AV1/JSON_files/video.JSON', 'code/codecs/AV1/JSON_files/svt.JSON')
-x.encode()
-x.decode()
-x.parsed2csv()
+        BR_STRING = 'Total Frames	Average QP  	Y-PSNR   	U-PSNR   	V-PSNR		| 	Y-PSNR   	U-PSNR   	V-PSNR   	|	Y-SSIM   	U-SSIM   	V-SSIM   	|	Bitrate\n'
+        with open(pt1, 'r') as output_text:
+            out_string = output_text.readlines()
+            results_index = (out_string.index(BR_STRING) + 1)
+            bitrate_string = out_string[results_index].split()[20]
+            psnr_string = out_string[results_index].split()[2]
+        with open(pt2, 'rt') as outtime_text:
+            outtime_string = outtime_text.readlines()
+        for strtime in outtime_string:
+            if not strtime.startswith("Total Encoding Time"):
+                continue
+            timems_string = strtime.split()[3]
+        return float(bitrate_string)*1024, float(psnr_string) , float(timems_string)
