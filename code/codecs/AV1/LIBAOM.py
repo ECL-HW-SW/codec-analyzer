f7f493dc03f95e2e9315e0d343f675cfb8f259a2
EduLodi <f.eduardo.lodi@gmail.com>
2022-09-12 12:22:46 -0300

    Fixed some issues, moved some function to their classes, start of standardization of JSON files

--- code/codecs/AV1/LIBAOM.py
+++ code/codecs/AV1/LIBAOM.py
@@ -1,6 +1,5 @@
 import os
 import json
-import av1_outparse
 import csv
 from pathlib import Path
 
@@ -37,7 +36,7 @@ class libaom_codec:
         outgen = self.data['paths']['outgen'] +"/aom"+self.name
         p = Path('~').expanduser()
         outgen = outgen.replace("~",str(p))
-        bitrate, psnr, timems = av1_outparse.parse_aom_output(outgen)
+        bitrate, psnr, timems = self.parse_aom_output(outgen)
         return bitrate,psnr,timems
 
     def parsed2csv(self):
@@ -50,3 +49,13 @@ class libaom_codec:
             metrics_writer = csv.writer(metrics_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             metrics_writer.writerow(['encoder','video','resolution','fps','number of frames','qp','bitrate', 'psnr', 'timems','optional settings'])
             metrics_writer.writerow(["LIBAOM-AV1",self.name,self.__raw["resolution"],self.__raw["fps"],self.__raw["framesnumber"],'',bitrate,psnr,timems,self.data["paths"]["options_encoder"]])
+
+    def parse_aom_output(st):
+        with open(st, 'r') as output_text:
+            out_string = output_text.readlines()
+            for line in out_string:
+                if (line.startswith("Stream")):
+                    bitrateaom_string = line.split()[9]
+                    psnr_stringaom = line.split()[5]
+                    ms_stringaom = line.split()[11]
+        return float(bitrateaom_string), float(psnr_stringaom), float(ms_stringaom)        
