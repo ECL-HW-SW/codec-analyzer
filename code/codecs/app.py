b6e7a8522a45705fcef6427a3b6fcf184ee21ba1
Arthur <104280700+scarpart@users.noreply.github.com>
2022-09-23 19:46:26 -0300

    updated app.py to include VVcodec. Test prints now include all 3 in 3 pairs.

--- code/codecs/app.py
+++ code/codecs/app.py
@@ -1,21 +1,28 @@
 from EVC import EVC
 from CodecComparator import CodecComparator
 from SVT import svt_codec
+from VVcodec import VVcodec
 qps = [22,27,32,37]
 
 svt = svt_codec()
 evc = EVC()
-# for qp in qps:
-#     svt.set_qp(qp)
-#     svt.encode()
-#     # a = svt.parse()
-#     svt.add_to_csv()
-#     evc.set_qp(qp)
-#     evc.encode()
-#     a = evc.parse()
-#     evc.add_to_csv(a)
-
+vvenc = VVcodec()
+for qp in qps:
+    svt.set_qp(qp)
+    svt.encode()
+    svt.add_to_csv()
+    
+    vvenc.set_qp(qp)
+    vvenc.encode()
+    vvenc.add_to_csv
+    
+    # a = svt.parse()
+    evc.set_qp(qp)
+    evc.encode()
+    a = evc.parse()
+    evc.add_to_csv(a)
 
 comp = CodecComparator()
-print(comp.bdrate(svt.get_csvs(),evc.get_csvs()))
-print(comp.bdrate(evc.get_csvs(),svt.get_csvs()))
+print("SVT-EVC BDRATE:", comp.bdrate(svt.get_csvs(), evc.get_csvs()))
+print("VVENC-EVC BDRATE:", comp.bdrate(vvenc.get_csvs(), evc.get_csvs()))
+print("VVENC-SVT BDRATE:", comp.bdrate(vvenc.get_csvs(), evc.get_csvs()))
