c3171ead3f83d4cfe5bb5a833a1627586d8e2110
scarpart <aenartzr@gmail.com>
2022-12-23 15:36:16 -0300

    HttpContent now supports DELETE and PUT - all tried and tested, both work well

--- HttpContent.py
+++ HttpContent.py
@@ -10,31 +10,70 @@ class HttpContent:
     so that it can then be parsed as JSON and sent.
     """
 
+
     def __init__(self, base_url: str) -> None:
         self.base_url = base_url
+
 
     def GET(self) -> list:
         response = requests.get(self.base_url)
         return ast.literal_eval(response.text)
 
-    def POST(self, codec: str, video: str, resolution: str,
-                fps: float, nframes: int, qp: int, ypsnr: float,
-                upsnr: float, vpsnr: float, yuvpsnr: float,
-                bitrate: float, time: float) -> requests.Response:
+
+    def POST(self, info: dict) -> requests.Response:
         payload = json.dumps({
-            "codec": codec,
-            "video": video,
-            "resolution": resolution,
-            "fps": fps,
-            "nFrames": nframes,
-            "qp": qp,
-            "yPSNR": ypsnr,
-            "uPSNR": upsnr,
-            "vPSNR": vpsnr,
-            "yuvPSNR": yuvpsnr,
-            "bitrate": bitrate,
-            "time": time
+            "codec": info["codec"],
+            "video": info["video"],
+            "resolution": info["resolution"],
+            "fps": info["fps"],
+            "nFrames": info["nFrames"],
+            "qp": info["qp"],
+            "yPSNR": info["ypsnr"],
+            "uPSNR": info["upsnr"],
+            "vPSNR": info["vpsnr"],
+            "yuvPSNR": info["yuvpsnr"],
+            "bitrate": info["bitrate"],
+            "time": info["time"]
         })
         headers = {'Content-Type': 'application/json'}
         response = requests.request("POST", self.base_url, headers=headers, data=payload)
         return response
+    
+
+    def PUT(self, id: str, info: dict) -> requests.Response:
+        payload = json.dumps({
+            "codec": info["codec"],
+            "video": info["video"],
+            "resolution": info["resolution"],
+            "fps": info["fps"],
+            "nFrames": info["nFrames"],
+            "qp": info["qp"],
+            "yPSNR": info["ypsnr"],
+            "uPSNR": info["upsnr"],
+            "vPSNR": info["vpsnr"],
+            "yuvPSNR": info["yuvpsnr"],
+            "bitrate": info["bitrate"],
+            "time": info["time"]
+        })
+        url = self.base_url + f"/{id}"
+        headers = {'Content-Type': 'application/json'}
+        response = requests.request("PUT", url, headers=headers, data=payload)
+        return response
+    
+
+    def DELETE(self, id: str) -> requests.Response:
+        url = self.base_url + f"/{id}"
+        headers = {'Content-Type': 'application/json'}
+        response = requests.request("DELETE", url, headers=headers)
+        return response
+
+    
+    """
+    GETTERS AND SETTERS BELOW
+    """
+
+    def set_base_url(self, base_url: str) -> None:
+        self.base_url = base_url
+
+    def get_base_url(self) -> str:
+        return self.base_url
