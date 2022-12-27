6e72100e6be08882e0aab8166fc4d29460565589
scarpart <aenartzr@gmail.com>
2022-12-26 22:01:30 -0300

    added some attributes and methods, as well as standard getters and setters to VVcodec and parent class

--- video_codecs/Codec.py
+++ video_codecs/Codec.py
@@ -3,13 +3,14 @@ import json
 from GlobalPaths import GlobalPaths
 import os
 
-class Codec(ABC):
-    def __init__(self, codec, codec_config):
-        self._codec = codec
-        paths = GlobalPaths().get_paths()
 
-        # TODO: add hash dos codecs (erro se n tiver)
- 
+
+class Codec(ABC):
+
+    def __init__(self, codec, codec_config, commit_hash):
+        self._codec = codec
+        self._commit_hash = commit_hash
+        paths = GlobalPaths().get_paths()
         with open(codec_config, "r") as json_file:
             data = json.load(json_file)
             self._encoder_path = os.path.join(paths["codecs_path"], data['encoder_path'])
@@ -17,42 +18,62 @@ class Codec(ABC):
             self._options_encoder = data['options_encoder']
             self._options_decoder = data['options_decoder']
 
+    # TODO: add hash dos codecs (erro se n tiver)
 
-    def get_codec(self):
+    """GETTERS"""
+    def get_codec(self) -> str:
         return self._codec
-    def get_encoder_path(self):
+
+    
+    def get_commit_hash(self) -> str:
+        return self._commit_hash
+
+
+    def get_encoder_path(self) -> str:
         return self._encoder_path
-    def get_decoder_path(self):
+    
+    
+    def get_decoder_path(self) -> str:
         return self._decoder_path
     
-##### setters ##########################
-    def set_encoder_option(self, name, val):
+
+    """SETTERS"""
+    def set_encoder_option(self, name, val) -> None:
         self._options_encoder[name] = val
    
-    def set_decoder_option(self, name, val):
+    
+    def set_decoder_option(self, name, val) -> None:
         self._options_encoder[name] = val
 
+
+    def set_commit_hash(self, commit_hash: str) -> None:
+        self._commit_hash = commit_hash
+
+    """ABSTRACT METHODS"""
     @abstractmethod
     def set_qp(self, val):
         pass
+    
     
     @abstractmethod
     def set_num_frames(self, val):
         pass
 
-##### end of section ##################   
 
     @abstractmethod
     def encode(self):
         pass
 
+
     @abstractmethod
     def decode(self):
         pass
 
+
     @abstractmethod
     def parse(self):
         pass
+
 
     @abstractmethod
     def add_to_csv(self):
--- video_codecs/VVcodec.py
+++ video_codecs/VVcodec.py
@@ -8,9 +8,22 @@ from Logger import Logger
 
 class VVcodec(Codec):
     
-    def __init__(self, config_path):
-        super().__init__("vvcodec", config_path)
+    def __init__(self, config_path, commit_hash, video):
+        super().__init__("vvcodec", config_path, commit_hash=commit_hash)
+        self._video = video
     
+    
+    # TODO: transfer these getters and setters to the parent class if possible to reduce repetition
+    # -----------------------------------------------------------------------
+    # FIXME: not really anything to fix, i just needed a colourful annotation
+    # things i've changed:
+    #    1. Video class is now passed in as a constructor parameter, and changing the current video is done via setter
+    #        --> this is so the method get_unique_config() works separately from encode()
+    #    2. added get_preset() 
+    #    3. added more information to the unique_config, for better database integration
+    #    4. added get_unique_config() to return the unique_config
+    #    5. added hasEntry to HttpContent to make use of the bool and check on app.py whether it should encode with those unique_configs
+
 
     def set_qp(self, val):
         self._options_encoder["qp"] = val
@@ -26,10 +39,29 @@ class VVcodec(Codec):
 
     def set_preset(self, val):
         self._options_encoder["preset"] = val
+
+    
+    def get_preset(self) -> str:
+        return self._options_encoder["preset"]
     
 
     def get_num_frames(self):
         return self._options_encoder["frames"]
+
+
+    def get_unique_config(self) -> str:
+        return "_".join([
+            self._video.get_name(), str(self.get_qp()) + "qp", str(self.get_num_frames()) + "fr",
+            self._video.get_fps() + "fps", self.get_preset() + "-preset", self.get_threads() + "t"
+        ])
+
+    
+    def set_video(self, video) -> None:
+        self._video = video
+
+
+    def get_video(self):
+        return self._video
 
 
     def encode(self, video, force_rerun = 0) -> str: 
@@ -41,7 +73,12 @@ class VVcodec(Codec):
         for key, val in self._options_encoder.items():
             options_str += f"--{key} {val} "
 
-        base_output_name = "_".join([video.get_name(), str(self.get_qp()) + "qp", str(self.get_num_frames()) + "fr"])
+        # This relates to the "uniqueConfig" column in the codec-database
+        # This, together with the commitHash, should be a unique combination that only occurs once in the DB
+        # ex: bowing_22qp_30fr_29.97fps_fast-preset
+        
+        base_output_name = self.get_unique_config()
+        log.info(base_output_name)
         self.__bitstream_path = os.path.join(paths[self._codec]["bitstream_dir"], base_output_name + ".bin")
         self.__report_path = os.path.join(paths[self._codec]["report_dir"], base_output_name + ".txt")
         self.__csv_path = os.path.join(paths[self._codec]["csv_dir"], base_output_name + ".csv")
@@ -56,15 +93,16 @@ class VVcodec(Codec):
 
         part1 = f'{self._encoder_path} -i {video.get_abs_path()} -q {self.get_qp()} {options_str} '
         part2 = f'--output {self.__bitstream_path} '
-        part3 = f'> {self.__report_path}' # TODO: mudar isso dps
-        cmdline  =part1+part2+part3 
+        part3 = f'> {self.__report_path}' # TODO: mudar isso dps (o que?)
+        cmdline = part1+part2+part3 
 
         os.system(cmdline)
         log.info(cmdline) 
 
 
     def decode(self):
-        print("\nDECODING VVCODEC...\n")
+        log = Logger()
+        log.info("\nDECODING VVCODEC...\n")
 
         p = str(Path('~').expanduser())
 
@@ -74,14 +112,14 @@ class VVcodec(Codec):
         bitstream_path = self.get_bitstream()
         bitstream_path = bitstream_path.replace('~', p)
         if not os.path.exists(bitstream_path):
-            print("Bitstream path does not exist.")
+            log.info("Bitstream path does not exist.")
 
         part1 = f'{self.get_decoder()} -b {bitstream_path}/vvcodec_{self.get_videoname()}_{self.get_qp()} {self.get_options_decoder()} '
         part2 = f'-v 0 -f {self.get_framesnumber()} -o {decoded_path}/vvcodec_{self.get_videoname()}_{self.get_qp()}'
         cmdline = part1+part2 
 
         os.system(cmdline)
-        print(cmdline) 
+        log.info(cmdline) 
 
 
     def parse(self) -> tuple:
@@ -146,11 +184,13 @@ class VVcodec(Codec):
             metrics_writer = csv.writer(metrics_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             metrics_writer.writerow(['encoder','video','resolution','fps','number of frames','qp','bitrate', 'psnr', 'timems','optional settings'])
             metrics_writer.writerow(["VVENC",video.get_name(),video.get_resolution(),video.get_fps(),
-                                        video.get_framesnumber(),self.get_qp(),bitrate,psnr,timems,self._options_encoder])
+                                        self.get_num_frames(),self.get_qp(),bitrate,psnr,timems,self._options_encoder])
+            # TODO: I CHANGED video.get_framesnumber() ABOVE TO self.get_num_frames() --
+            # CHECK IF THIS IS THE RIGHT THING TO DO OR NOT (I THINK IT IS)
             metrics_file.close()
 
 
-    def get_encoding_info(self, video) -> dict:
+    def get_encoding_info(self) -> dict:
         """
         Creates a dictionary with the same encoding info that is written to the csv file
         so that it can be sent in an HTTP request and stored into the database
@@ -158,16 +198,18 @@ class VVcodec(Codec):
 
         bitrate, yuvpsnr, timems, ypsnr, upsnr, vpsnr = self.parse_extra()
         info_vvenc = {
-            "codec": "vvenc",
-            "video": video.get_name(),
-            "resolution": video.get_resolution(),
-            "fps": video.get_fps(),
-            "nFrames": video.get_framesnumber(),
-            "qp": video.get_qp(),
-            "yPSNR": ypsnr,
-            "uPSNR": upsnr,
-            "vPSNR": vpsnr,
-            "yuvPSNR": yuvpsnr,
+            "codec": self.get_codec(),
+            "commitHash": self.get_commit_hash(),
+            "uniqueConfig": self.get_unique_config(),
+            "video": self._video.get_name(),
+            "resolution": self._video.get_resolution(),
+            "fps": self._video.get_fps(),
+            "nFrames": self.get_num_frames(),
+            "qp": self.get_qp(),
+            "ypsnr": ypsnr,
+            "upsnr": upsnr,
+            "vpsnr": vpsnr,
+            "yuvpsnr": yuvpsnr,
             "bitrate": bitrate,
             "time": timems }
         return info_vvenc
@@ -177,8 +219,10 @@ class VVcodec(Codec):
         self.__threads = str(threads)
         self._options_encoder["--threads"] = str(threads)
     
+
     def get_threads(self) -> str:
         return self.__threads
+
 
     def get_csv_path(self):
         return self.__csv_path
